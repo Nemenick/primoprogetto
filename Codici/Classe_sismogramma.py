899e3ef2af82ecf552f2c3b3ff676819f0460990
Nemenick <giovanni.messuti.98@gmail.com>
2022-07-14 13:40:50 +0200

    modifica

--- Codici/Classe_sismogramma.py
+++ Codici/Classe_sismogramma.py
@@ -9,33 +9,56 @@ class Classe_Dataset:
         self.percorsocsv = percorsocsv
         datd = dd.read_csv(self.percorsocsv, usecols=coltot)
         self.allmetadata = {}
-        for i in coltot:
+        for i in coltot:                # genera metadata["colname"] = np.array["colname"]
             self.allmetadata[i] = np.array(datd[i])
-        # mi faccio il dizionario metadata["tracename"][1] etc
+        for key in self.allmetadata:
+            print(key, self.allmetadata[key])
+        # creo il dizionario metadata["tracename"][1] etc
         # print(self.metadata["trace_name"])
 
-    def selezioneDati(self, percorsohdf5):  # colselezione faccio selezione solo dei dati che servono
+    def acquisisci(self, percorsohdf5):  # colselezione faccio selezione solo dei dati che servono
         filehdf5 = h5py.File(percorsohdf5, 'r')
         dataset = filehdf5.get("data")
         print("\ndatasetORI", dataset)
         # nomidata = list(dataset.keys())                                  # Mi sono salvato i nomi di tutti i dataset
         nomidata = self.allmetadata["trace_name"]
-        print(nomidata[2])
-        sismogramma = []
+        print(nomidata)
+        self.sismogramma = []
         self.metadata = {}
         for key in self.allmetadata:            # creo dataset selezionato ma che ha gli stessi metadata del completo
             self.metadata[key] = []
         for i in range(len(nomidata)):
-            if (self.allmetadata["trace_polarity"][i] != 'undecidable'):        # TODO condizione da aggiornare
-                sismogramma.append(dataset.get(nomidata[i]))
+            if self.allmetadata["trace_polarity"][i] != 'undecidable' \
+                    and (self.allmetadata["station_channels"][i]=="HH" or self.allmetadata["station_channels"][i]=="EH"  ):        # TODO condizione da aggiornare
+                self.sismogramma.append(dataset.get(nomidata[i]))
+                self.sismogramma[-1] = self.sismogramma[-1][2]  # Componente z, ci mette di meno
+                for key in self.metadata:
+                    self.metadata[key].append(self.allmetadata[key][i])
+                    # P_times.append(trace_P_arrival_sample[i])  # Solo quelli con polarity definita
+                    # P_polarity.append(trace_polarity[i])
+        sismogramma = np.array(self.sismogramma)
+        print("shape", sismogramma.shape, len(self.metadata["trace_P_arrival_sample"]))
+
+    def plotta(self,visualizza):
+        for i in range(visualizza):                 #TODO aggiungi qui sotto un # per far printare traccia tutta
+            plt.plot(range(200), self.sismogramma[i][self.metadata["trace_P_arrival_sample"][i] - 100:self.metadata["trace_P_arrival_sample"][i] + 100])
+            plt.axvline(x=100, c="r", ls="--")
+            stringa = ""
             for key in self.metadata:
-                self.metadata[key].append(self.allmetadata[key][i])
-                # P_times.append(trace_P_arrival_sample[i])  # Solo quelli con polarity definita
-                # P_polarity.append(trace_polarity[i])
-                sismogramma[-1] = sismogramma[-1][2]  # TODO incredibilmente ci mette di meno!
-        sismogramma = np.array(sismogramma)
+                stringa = stringa + str(self.metadata[key][i]) + " "
+            plt.title(stringa)
+            plt.savefig("Metodo1_centrato_"+str(i))
+            plt.clf()
+            #plt.show()
+
 
 csv = 'C:/Users/HP_i3-7200U/Desktop/Simple_dataset/metadata/metadata_Instance_events_10k.csv'
-coltot = ["trace_name", "station_channels", "trace_P_arrival_sample", "trace_polarity"] # trace_name,station_channels needed
+hdf5 = 'C:/Users/HP_i3-7200U/Desktop/Simple_dataset/data/Instance_events_counts_10k.hdf5'
+coltot = ["trace_name", "station_channels", "trace_P_arrival_sample", "trace_polarity",
+          "trace_P_uncertainty_s","source_magnitude","source_magnitude_type"]
+# trace_name,station_channels needed
 Dataset_1 = Classe_Dataset()
 Dataset_1.letturacsv(csv, coltot)
+Dataset_1.acquisisci(hdf5)
+Dataset_1.plotta(visualizza=1058)
+
---
+++ Codici/Tutorial_Dizionari.py
@@ -0,0 +1,12 @@
+
+a = {}
+a["k2"] = []
+a["k2"].append(2)
+
+for key in a:
+    print(key)
+
+b = {}
+for key in a:
+    b[key] = []
+
