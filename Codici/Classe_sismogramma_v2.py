db6b87282eff4406c9076eb5b00fb88cf732268b
Nemenick <giovanni.messuti.98@gmail.com>
2022-07-20 12:52:10 +0200

    a

--- Codici/Classe_sismogramma_v2.py
+++ Codici/Classe_sismogramma_v2.py
@@ -20,7 +20,7 @@ class Classe_Dataset:
         # creo il dizionario metadata["tracename"][1] etc
         # print(self.metadata["trace_name"])"""
 
-    def acquisisci_new(self, percorsohdf5, percorsocsv, coltot):
+    def acquisisci_new(self, percorsohdf5, percorsocsv, coltot, nomi_selezionati):
         """
         Acquisisce e seleziona tracce del file hdf5 e csv
         e salva in file csv nomi e indici delle tracce selezionate
@@ -30,7 +30,7 @@ class Classe_Dataset:
         start = time.perf_counter()
         # FIXME engine"python" (lentissimo): dava problemi la riga 33114, l'ho skippata e legge ma dice che e too large-
         # datd = dd.read_csv(self.percorsocsv, usecols=coltot, engine="python", on_bad_lines="skip", sample=10**8, assume_missing=True)
-        datd = dd.read_csv(self.percorsocsv, usecols=coltot, engine="python", on_bad_lines="skip")
+        datd = dd.read_csv(self.percorsocsv, usecols=coltot)
         print(time.perf_counter() - start)
         self.allmetadata = {}
         for i in coltot:  # genera metadata["colname"] = np.array["colname"]
@@ -66,7 +66,7 @@ class Classe_Dataset:
         self.indice_csv = np.array(self.indice_csv)
         pd_names = pd.DataFrame({"trace_name": self.metadata["trace_name"],
                                  "indice_csv": self.indice_csv})  # basta indice_csv
-        pd_names.to_csv("Selezionati.csv", index=False)
+        pd_names.to_csv(nomi_selezionati, index=False)
         # pd_names = tracenames':[], 'indice file csv'
         print("shape", self.sismogramma.shape, len(self.metadata["trace_P_arrival_sample"]))
 
@@ -206,24 +206,25 @@ class Classe_Dataset:
             # plt.show()
 
 if __name__ == "main":
-    csvin = 'C:/Users/GioCar/Desktop/Simple_dataset/metadata/metadata_Instance_events_10k.csv'
-    hdf5in = 'C:/Users/GioCar/Desktop/Simple_dataset/data/Instance_events_counts_10k.hdf5'
-    csvout = 'C:/Users/GioCar/Desktop/Simple_dataset/metadata_Instance_events_selected_Polarity_Velocimeter.csv'
-    hdf5out = 'C:/Users/GioCar/Desktop/Simple_dataset/data_selected_Polarity_Velocimeter.hdf5'
-    txt_data = "C:/Users/GioCar/Desktop/txt_tracce.txt"
-    txt_metadata = "C:/Users/GioCar/Desktop/txt_metadata.txt"
-    coltot = ["trace_name", "station_channels", "trace_P_arrival_sample", "trace_polarity",
-              "trace_P_uncertainty_s", "source_magnitude", "source_magnitude_type"]
-    nomi = "Selezionati.csv"
-
-    # trace_name,station_channels needed
+    print("ci")
+    # csvin = 'C:/Users/GioCar/Desktop/Simple_dataset/metadata/metadata_Instance_events_10k.csv'
+    # hdf5in = 'C:/Users/GioCar/Desktop/Simple_dataset/data/Instance_events_counts_10k.hdf5'
+    # csvout = 'C:/Users/GioCar/Desktop/Simple_dataset/metadata_Instance_events_selected_Polarity_Velocimeter.csv'
+    # hdf5out = 'C:/Users/GioCar/Desktop/Simple_dataset/data_selected_Polarity_Velocimeter.hdf5'
+    # txt_data = "C:/Users/GioCar/Desktop/txt_tracce.txt"
+    # txt_metadata = "C:/Users/GioCar/Desktop/txt_metadata.txt"
+    # coltot = ["trace_name", "station_channels", "trace_P_arrival_sample", "trace_polarity",
+    #           "trace_P_uncertainty_s", "source_magnitude", "source_magnitude_type"]
+    # nomi = "Selezionati.csv"
+    #
+    # # trace_name,station_channels needed
     # Dataset_1 = Classe_Dataset()
-    # Dataset_1.to_txt(hdf5in, csvin, ["trace_name", "station_channels", "trace_P_arrival_sample",
-    #                                  "trace_polarity", "source_magnitude"], txt_data, txt_metadata)
-    # Dataset_1.leggi_custom_dataset(hdf5out,csvout)
-    # Dataset_1.crea_custom_dataset(hdf5in,csvin,hdf5out,csvout,coltot=coltot)
-    # Dataset_1.letturacsv(csv, coltot)
-    # Dataset_1.acquisisci_new(percorsohdf5=hdf5, percorsocsv=csv, coltot=coltot)
-    # Dataset_1.plotta(visualizza=30, namepng="Dataset_counts")
-    # Dataset_1.acquisisci_old(percorsohdf5=hdf5, percorsocsv=csv, coltot=coltot, percorso_nomi=nomi)
-    # Dataset_1.plotta(visualizza=5, namepng="/home/silvia/Desktop/Figure_Large_Custom_dataset/Custom_Large_dataset")
+    # # Dataset_1.to_txt(hdf5in, csvin, ["trace_name", "station_channels", "trace_P_arrival_sample",
+    # #                                  "trace_polarity", "source_magnitude"], txt_data, txt_metadata)
+    # # Dataset_1.leggi_custom_dataset(hdf5out,csvout)
+    # # Dataset_1.crea_custom_dataset(hdf5in,csvin,hdf5out,csvout,coltot=coltot)
+    # # Dataset_1.letturacsv(csv, coltot)
+    # Dataset_1.acquisisci_new(percorsohdf5=hdf5in, percorsocsv=csvin, coltot=coltot, nomi_selezionati=nomi)
+    # # Dataset_1.plotta(visualizza=30, namepng="Dataset_counts")
+    # # Dataset_1.acquisisci_old(percorsohdf5=hdf5, percorsocsv=csv, coltot=coltot, percorso_nomi=nomi)
+    # # Dataset_1.plotta(visualizza=5, namepng="/home/silvia/Desktop/Figure_Large_Custom_dataset/Custom_Large_dataset")
--- Codici/SimpleDataset_conv.py
+++ Codici/SimpleDataset_conv.py
@@ -1,9 +1,36 @@
+import numpy as np
 from tensorflow import keras
-from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten
+from keras.layers import Dense, Conv1D, MaxPooling1D, Flatten
 from keras.utils.np_utils import to_categorical
 from matplotlib import pyplot as plt
 from Classe_sismogramma_v2 import Classe_Dataset
 
 Dati = Classe_Dataset()
+csvin = 'C:/Users/GioCar/Desktop/Simple_dataset/metadata/metadata_Instance_events_10k.csv'
+hdf5in = 'C:/Users/GioCar/Desktop/Simple_dataset/data/Instance_events_counts_10k.hdf5'
+coltot = ["trace_name", "station_channels", "trace_P_arrival_sample", "trace_polarity",
+          "trace_P_uncertainty_s", "source_magnitude", "source_magnitude_type"]
+nomi = "Selezionati.csv"
+Dati.acquisisci_old(hdf5in, csvin, coltot=coltot, percorso_nomi=nomi)
 
-Dati
+x = np.zeros((len(Dati.sismogramma), 200))
+for i in range(len(Dati.sismogramma)):
+    x[i] = Dati.sismogramma[i][Dati.metadata["trace_P_arrival_sample"][i] - 100:
+                                              Dati.metadata["trace_P_arrival_sample"][i] + 100]
+print(type(x), x.shape)
+
+model = keras.models.Sequential([
+    Conv1D(16, 3, input_shape=(len(Dati.sismogramma[0]),  1), activation="relu"),
+    MaxPooling1D(2),
+    Conv1D(32, 3, activation="relu"),
+    MaxPooling1D(2),
+    Flatten(),
+    Dense(20, activation="relu"),
+    Dense(10, activation="softmax")
+])
+
+model.compile(
+    optimizer="adam",
+    loss="binary_crossentropy",
+    metrics=['accuracy']
+)
