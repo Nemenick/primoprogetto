56950d840bb74c41ecd2c1c45e04d9446719557a
Nemenick <giovanni.messuti.98@gmail.com>
2022-07-21 14:45:33 +0200

    gpu?

--- Codici/GPU_test.py
+++ Codici/GPU_test.py
@@ -1,7 +1,19 @@
+import tensorflow as tf
 
 print("\n\n\nINIZIO")
-devices = tf.config.list_physical_devices('GPU')
-print(len(devices))
+tf.test.is_gpu_available()
+print("aaaa")
+print(tf.debugging.set_log_device_placement(True))
+a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
+c = tf.matmul(a, b)
+
+print(c)
+
+
+devices = tf.config.list_physical_devices()
+print(devices)
 tf.test.is_built_with_cuda()
 print(tf.test.is_built_with_cuda())
+print(tf.config.list_physical_devices(device_type=None))
 print("\n\n\nFINE")
