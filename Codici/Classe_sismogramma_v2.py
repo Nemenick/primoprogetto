053801c6c9b4b2d2f9b70a363048277b457975a8
Nemenick <giovanni.messuti.98@gmail.com>
2022-07-21 15:10:22 +0200

    7

--- Codici/Classe_sismogramma_v2.py
+++ Codici/Classe_sismogramma_v2.py
@@ -23,7 +23,7 @@ class Classe_Dataset:
     def acquisisci_new(self, percorsohdf5, percorsocsv, coltot, nomi_selezionati):
         """
         Acquisisce e seleziona tracce del file hdf5 e csv
-        e salva in file csv nomi e indici delle tracce selezionate
+        e salva in file csv nomi_selezionati e indici delle tracce selezionate
         ATTENTO! (non creo un custom dataset di trace, ma solo salvo in csv lista di quelle da leggere)
         """
         self.percorsocsv = percorsocsv
@@ -44,17 +44,17 @@ class Classe_Dataset:
 
         filehdf5 = h5py.File(percorsohdf5, 'r')
         dataset = filehdf5.get("data")
-        print("\ndatasetORI", dataset)
+        # print("\ndatasetORI", dataset)
         # nomidata = list(dataset.keys())                             # Mi sono salvato i nomi di tutti i dataset
         nomidata = self.allmetadata["trace_name"]                     # Presi dal file CSV
-        print(nomidata)
+        # print(nomidata)
         self.sismogramma = []
         self.metadata = {}
         self.indice_csv = []
         for key in self.allmetadata:            # creo dataset selezionato ma che ha gli stessi metadata del completo
             self.metadata[key] = []
         for i in range(len(nomidata)):
-            if self.allmetadata["trace_polarity"][i] != 'undecidable' \
+            if self.allmetadata["trace_polarity"][i] == 'negative' \
                     and (self.allmetadata["station_channels"][i] == "HH" or
                          self.allmetadata["station_channels"][i] == "EH"):        # TODO condizione da aggiornare
                 self.sismogramma.append(dataset.get(nomidata[i]))
@@ -62,6 +62,11 @@ class Classe_Dataset:
                 self.indice_csv.append(i)
                 for key in self.metadata:
                     self.metadata[key].append(self.allmetadata[key][i])
+        print("\nshape_prima di ridimensionare\n",len(self.sismogramma),len(self.sismogramma[0]))
+        for i in range(len(self.sismogramma)):
+            self.sismogramma[i] = self.sismogramma[i][self.metadata["trace_P_arrival_sample"][i]-200:   # TODO windows
+                                                   self.metadata["trace_P_arrival_sample"][i]+200]
+
         self.sismogramma = np.array(self.sismogramma)
         self.indice_csv = np.array(self.indice_csv)
         pd_names = pd.DataFrame({"trace_name": self.metadata["trace_name"],
@@ -179,8 +184,8 @@ class Classe_Dataset:
             print("ho caricato la key ", key, time.perf_counter() - start)
         print(self.sismogramma.shape, len(self.sismogramma))
 
-    def to_txt(self, percorsohdf5, percorsocsv, coltot, txt_data, txt_metadata):
-        self.acquisisci_new(percorsohdf5, percorsocsv, coltot=coltot)
+    def to_txt(self, percorsohdf5, percorsocsv, coltot, nomi_selezionati, txt_data, txt_metadata):
+        self.acquisisci_new(percorsohdf5, percorsocsv, coltot=coltot, nomi_selezionati=nomi_selezionati)
         # print("\n\nVA BENE?", self.sismogramma)
         np.savetxt(txt_data, self.sismogramma, fmt='%.5e')
         metadata_txt = pd.DataFrame.from_dict(self.metadata)
@@ -205,6 +210,13 @@ class Classe_Dataset:
             plt.clf()
             # plt.show()
 
+csvin = '/home/silvia/Desktop/Sample_dataset/metadata/metadata_Instance_events_10k.csv'
+hdf5in = '/home/silvia/Desktop/Sample_dataset/data/Instance_events_counts_10k.hdf5'
+txt_data = "/home/silvia/Desktop/txt_tracce_down.txt"
+txt_metadata = "/home/silvia/Desktop/txt_metadata_down.txt"
+Dataset_1 = Classe_Dataset()
+Dataset_1.to_txt(hdf5in, csvin, ["trace_name", "station_channels", "trace_P_arrival_sample","trace_polarity",
+                                 "source_magnitude"], "selezionati", txt_data, txt_metadata)
 if __name__ == "main":
     print("ci")
     # csvin = 'C:/Users/GioCar/Desktop/Simple_dataset/metadata/metadata_Instance_events_10k.csv'
@@ -217,14 +229,14 @@ if __name__ == "main":
     #           "trace_P_uncertainty_s", "source_magnitude", "source_magnitude_type"]
     # nomi = "Selezionati.csv"
     #
-    # # trace_name,station_channels needed
+    # trace_name,station_channels needed
     # Dataset_1 = Classe_Dataset()
-    # # Dataset_1.to_txt(hdf5in, csvin, ["trace_name", "station_channels", "trace_P_arrival_sample",
-    # #                                  "trace_polarity", "source_magnitude"], txt_data, txt_metadata)
-    # # Dataset_1.leggi_custom_dataset(hdf5out,csvout)
-    # # Dataset_1.crea_custom_dataset(hdf5in,csvin,hdf5out,csvout,coltot=coltot)
-    # # Dataset_1.letturacsv(csv, coltot)
+    # Dataset_1.to_txt(hdf5in, csvin, ["trace_name", "station_channels", "trace_P_arrival_sample",
+    #                                  "trace_polarity", "source_magnitude"], txt_data, txt_metadata)
+    # Dataset_1.leggi_custom_dataset(hdf5out,csvout)
+    # Dataset_1.crea_custom_dataset(hdf5in,csvin,hdf5out,csvout,coltot=coltot)
+    # Dataset_1.letturacsv(csv, coltot)
     # Dataset_1.acquisisci_new(percorsohdf5=hdf5in, percorsocsv=csvin, coltot=coltot, nomi_selezionati=nomi)
-    # # Dataset_1.plotta(visualizza=30, namepng="Dataset_counts")
-    # # Dataset_1.acquisisci_old(percorsohdf5=hdf5, percorsocsv=csv, coltot=coltot, percorso_nomi=nomi)
-    # # Dataset_1.plotta(visualizza=5, namepng="/home/silvia/Desktop/Figure_Large_Custom_dataset/Custom_Large_dataset")
+    # Dataset_1.plotta(visualizza=30, namepng="Dataset_counts")
+    # Dataset_1.acquisisci_old(percorsohdf5=hdf5, percorsocsv=csv, coltot=coltot, percorso_nomi=nomi)
+    # Dataset_1.plotta(visualizza=5, namepng="/home/silvia/Desktop/Figure_Large_Custom_dataset/Custom_Large_dataset")
