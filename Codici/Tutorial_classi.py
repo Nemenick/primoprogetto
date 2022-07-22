60e84491ef19803f33bfe44a29ff8353258106dc
Nemenick <giovanni.messuti.98@gmail.com>
2022-07-22 12:41:42 +0200

    Create Tutorial_classi.py

---
+++ Codici/Tutorial_classi.py
@@ -0,0 +1,18 @@
+
+class Classemia:
+
+
+    def __init__(self):
+        self.centrato = False
+        a=3
+    def fun1(self):
+        a = 3
+        self.b=0
+    def fun2(self):
+        print(self.centrato)
+
+d=Classemia()
+d.fun2()
+d.fun1()
+print(d.b)
+print(d.a)
