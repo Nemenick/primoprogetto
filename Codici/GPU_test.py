a082d932cb6c141246fdf0d98960820010ad1271
Nemenick <giovanni.messuti.98@gmail.com>
2022-07-29 11:21:24 +0200

    Update GPU_test.py

--- Codici/GPU_test.py
+++ Codici/GPU_test.py
@@ -8,11 +8,10 @@ a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
 b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
 c = tf.matmul(a, b)
 
-print("c è questo",c)
+print("c è questo tensore", c)
 
 
 devices = tf.config.list_physical_devices()
-print("devices",devices)
-print("build cuda",tf.test.is_built_with_cuda())
-print("devices 2.0",tf.config.list_physical_devices(device_type=None))
+print("devices", devices)
+print("build cuda", tf.test.is_built_with_cuda())
 print("FINE\n\n\n")
