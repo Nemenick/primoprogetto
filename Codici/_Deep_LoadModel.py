f5d2f098247a507f77f171f957100517aef473a0
Nemenick <giovanni.messuti.98@gmail.com>
2022-10-04 13:07:01 +0200

    ok

--- Codici/_Deep_LoadModel.py
+++ Codici/_Deep_LoadModel.py
@@ -12,10 +12,13 @@ N_test = 500
 # hdf5in = 'C:/Users/GioCar/Desktop/Tesi_5/data_Velocimeter_Buone_normalizzate_4s.hdf5'
 # csvin = 'C:/Users/GioCar/Desktop/Tesi_5/metadata_Velocimeter_Buone_normalizzate1_4s.csv'
 
+hdf5in = '/home/silvia/Desktop/Pollino/Pollino_100Hz_data.hdf5'
+csvin = '/home/silvia/Desktop/Pollino/Pollino_100Hz_metadata.csv'
 
 Dati = ClasseDataset()
 Dati.leggi_custom_dataset(hdf5in, csvin)
-Dati.elimina_tacce_indici([124709])
+print("Nsismogrammi", len(Dati.sismogramma), "Npolarità", len(Dati.metadata["trace_polarity"]))
+# Dati.elimina_tacce_indici([124709])
 
 semiampiezza = 130
 lung = len(Dati.sismogramma[0])
@@ -23,15 +26,18 @@ lung = len(Dati.sismogramma[0])
 x = np.zeros((len(Dati.sismogramma), semiampiezza*2))
 for i in range(len(Dati.sismogramma)):
     x[i] = Dati.sismogramma[i][lung // 2 - semiampiezza:lung // 2 + semiampiezza]
-y = np.array([Dati.metadata["trace_polarity"][i] == "positive" for i in range(N_test)])
+y = np.array([Dati.metadata["trace_polarity"][i] == "positive" for i in range(len(Dati.sismogramma))])
 y = y + 0
 # model = keras.models.load_model('C:/Users/GioCar/Documents/GitHub/primoprogetto/Codici/Tentativi/2/Simple_data_conv_1.0.hdf5')
-model = keras.models.load_model('C:/Users/GioCar/Documents/GitHub/primoprogetto/Codici/Tentativi/2/Simple_data_conv_1.0.hdf5')
+model = keras.models.load_model('/home/silvia/Documents/GitHub/primoprogetto/Codici/Tentativi/4/Tentativo_4.hdf5')
 model.summary()
-yp = model.predict(x[0:N_test])
-print(y, "\n", yp)
-dizio = {"y_INGV": y, "y_predict": yp}
+yp = model.predict(x)
+print(y, len(y), "\n", yp, len(yp))
+yp_ok = []
+for i in yp:
+    yp_ok.append(i[0])
+dizio = {"y_a_Mano": y, "y_predict": yp_ok}
 datapandas = pd.DataFrame.from_dict(dizio)
-datapandas.to_csv('C:/Users/GioCar/Desktop/Predizioni.xls', index=False)
+datapandas.to_csv('/home/silvia/Desktop/Predizioni_Pollino.csv', index=False)
 
 print(type(yp))
